ebb88897b45241c27d3c003a2a5eff1d369f6ac4
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-01 10:22:07 +0200

    Added types to function names

--- src/app/components/api.service.ts
+++ src/app/components/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from './models/recipe.model';
+import { Observable } from 'rxjs';
 
 export const pageUrl = "http://localhost:4000";
 
@@ -10,23 +11,23 @@ export const pageUrl = "http://localhost:4000";
 export class ApiService {
   constructor(private http: HttpClient) { }
 
-  getAllRecipes() {
+  getAllRecipes(): Observable<Recipe[]> {
     return this.http.get<Recipe[]>(`${pageUrl}/api/recipes`);
   }
 
-  getRecipe(id: Number) {
+  getRecipe(id: Number): Observable<Recipe> {
     return this.http.get<Recipe>(`${pageUrl}/api/recipes/${id}`);
   }
 
-  addRecipe(formData, image: File) {
+  addRecipe(formData, image: File): Observable<Recipe> {
     const data = new FormData();
     data.append('json', JSON.stringify(formData));
     data.append('file', image);
 
-    return this.http.post(`${pageUrl}/api/upload`, data);
+    return this.http.post<Recipe>(`${pageUrl}/api/upload`, data);
   }
 
-  deleteRecipe(id: Number) {
-    return this.http.delete(`${pageUrl}/api/recipes/${id}`);
+  deleteRecipe(id: Number): Observable<Recipe> {
+    return this.http.delete<Recipe>(`${pageUrl}/api/recipes/${id}`);
   }
 }
