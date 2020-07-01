05dfe380641643c7fef04c359cd9a949f1bdbdb1
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-01 11:06:52 +0200

    Added edit and submit buttons and feature to change text into inputs on edit

--- src/app/components/recipe-page/recipe-page.component.ts
+++ src/app/components/recipe-page/recipe-page.component.ts
@@ -1,45 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from "./../api.service";
+import { ApiService } from './../api.service';
 import { Recipe } from './../models/recipe.model';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
-
-
-
 
 @Component({
   selector: 'app-recipe-page',
   templateUrl: './recipe-page.component.html',
-  styleUrls: ['./recipe-page.component.css']
+  styleUrls: ['./recipe-page.component.css'],
 })
-
-
-
 export class RecipePageComponent implements OnInit {
   recipe: Recipe;
+  public edit: boolean;
   public id: Number;
-  constructor(private recipesService: ApiService,
+  constructor(
+    private recipesService: ApiService,
     private route: ActivatedRoute,
     private router: Router
-    ) {
-    // this.recipe = ;
-  }
+  ) {}
 
   ngOnInit(): void {
+    this.edit = false;
     this.id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.recipesService.getRecipe(this.id).subscribe((recipe: any) =>
-    {
+    this.recipesService.getRecipe(this.id).subscribe((recipe: Recipe) => {
       this.recipe = recipe;
-      console.log(recipe)
-    })
+      console.log(recipe);
+    });
   }
 
   handleDelete() {
-    console.log(`handling delete: ${this.id}`)
-    this.recipesService.deleteRecipe(this.id).toPromise()
-    .then(() => alert("Deleted!"))
-    .then(() => this.router.navigate(['/']));
-
+    this.recipesService
+      .deleteRecipe(this.id)
+      .toPromise()
+      .then(() => alert('Deleted!'))
+      .then(() => this.router.navigate(['/']));
   }
 
+  handleEdit() {
+    console.log('Handling edit');
+    this.edit = !this.edit;
+  }
+  handleSubmit() {
+    console.log('Form is done!');
+    this.edit = false;
+  }
 }
