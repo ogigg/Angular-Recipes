257bb233571af4061f0e45d656e52fab6af3cba5
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-02 11:56:15 +0200

    add @ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecipePageComponent } from './components/recipe-page/recipe-page.component';
 import { AddRecipePageComponent } from './components/add-recipe-page/add-recipe-page.component';
 import { RecipeThumbnailComponent } from './components/recipe-thumbnail/recipe-thumbnail.component';
-import {AutosizeModule} from 'ngx-autosize';
+import { AutosizeModule } from 'ngx-autosize';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
 @NgModule({
@@ -24,7 +25,6 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     AddRecipePageComponent,
     RecipeThumbnailComponent,
     RecipeEditComponent,
-
   ],
   imports: [
     BrowserModule,
@@ -32,9 +32,20 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     NgbModule,
     HttpClientModule,
     ReactiveFormsModule,
-    AutosizeModule
+    AutosizeModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
