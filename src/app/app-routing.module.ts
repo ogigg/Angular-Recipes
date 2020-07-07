3ada442f9cdfd1b5e5421beb640821cdfd45f012
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-07 14:18:17 +0200

    Add login to routing, remove bug from header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddRecipePageComponent } from './components/add-recipe-page/add-recipe-page.component';
 import { RecipePageComponent } from './components/recipe-page/recipe-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
-
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'recipe/:id', component: RecipePageComponent },
   { path: 'add-recipe', component: AddRecipePageComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', component: HomePageComponent },
   { path: '**', component: HomePageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
