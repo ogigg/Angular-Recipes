431ef68e52251c67609ead45d40a676ed8437fc9
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-08 14:34:44 +0200

    Add redirect to login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TranslateService } from '@ngx-translate/core';
@@ -14,12 +14,19 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private authService: AuthService,
     private snackBar: MatSnackBar,
-    translate: TranslateService
+    translate: TranslateService,
+    activatedRoute: ActivatedRoute
   ) {
+    activatedRoute.queryParams.subscribe((params) => {
+      if (params.returnUrl) {
+        this.redirectUrl = params.returnUrl;
+      }
+    });
     translate.get('login-snackbar').subscribe((res: string) => {
       this.snackBarMessage = res;
     });
   }
+  private redirectUrl = '/';
   public hide: boolean = true;
   private snackBarMessage: string = '';
   ngOnInit(): void {}
@@ -30,7 +37,7 @@ export class LoginComponent implements OnInit {
       form.value.password
     );
     if (response.success === true) {
-      this.router.navigate(['/']);
+      this.router.navigate([this.redirectUrl]);
     } else {
       this.snackBar.open(this.snackBarMessage, 'OK', {
         duration: 2000,
