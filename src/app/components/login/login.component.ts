6ebb0ebe18570d5695ab67eefd942cfb3ce2085c
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-08 12:02:31 +0200

    Replace api service with auth service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ApiService } from '../api.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,20 +10,15 @@ import { ApiService } from '../api.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router, private apiService: ApiService) {}
+  constructor(private router: Router, private authService: AuthService) {}
   public hide: boolean = true;
 
   ngOnInit(): void {}
 
-  async login(form) {
-    console.log(form.value);
-    const response = await this.apiService
-      .login(form.value.email, form.value.password)
-      .toPromise();
-    if (response.status) {
-      alert('Zalogowano!');
-    } else {
-      alert('Złe dane');
-    }
+  login(form) {
+    const response = this.authService.login(
+      form.value.email,
+      form.value.password
+    );
   }
 }
