566c8a36f93b7ef8db7341148595cb3e301995a1
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-13 09:44:39 +0200

    Add checking is user is logged in on app start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Store } from '@ngrx/store';
+import { login } from './components/login/auth.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +9,16 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private store: Store) {
     translate.setDefaultLang(translate.getBrowserLang());
   }
   title = 'angular-recipes';
+
+  ngOnInit() {
+    const userProfile = localStorage.getItem('user');
+
+    if (userProfile) {
+      this.store.dispatch(login({ user: JSON.parse(userProfile) }));
+    }
+  }
 }
