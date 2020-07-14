47b250e1e78e68cc7e119d7e444d566ff70fea26
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-14 14:59:37 +0200

    Fix authGuard not detecting isAuthenticated properly

--- src/app/components/auth-guard.service.ts
+++ src/app/components/auth-guard.service.ts
@@ -6,18 +6,25 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { AuthService } from './auth.service';
+import { Store, select } from '@ngrx/store';
+import { selectIsLoggedIn } from './login/auth.selectors';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
-  constructor(public auth: AuthService, private router: Router) {}
-
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private store: Store
+  ) {}
+  private isAuthenticated: boolean = false;
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    if (this.auth.isAuthenticated()) {
+    this.store.pipe(select(selectIsLoggedIn)).subscribe(resp => this.isAuthenticated = resp);
+    if (this.isAuthenticated) {
       return true;
     } else {
       this.router.navigate(['login'], {
