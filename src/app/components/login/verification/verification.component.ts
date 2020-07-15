c3ae953c52a3d53c761b2aa2629ae90f7009bfd1
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-15 12:10:34 +0200

    Add inputs and layout, Add feature to move between inputs

--- src/app/components/login/verification/verification.component.ts
+++ src/app/components/login/verification/verification.component.ts
@@ -4,6 +4,8 @@ import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/reducers';
 import { selectUser } from '../auth.selectors';
 import { User } from '../../models/user.model';
+import { Observable, timer, interval } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-verification',
@@ -11,8 +13,13 @@ import { User } from '../../models/user.model';
   styleUrls: ['./verification.component.scss'],
 })
 export class VerificationComponent implements OnInit {
-  @ViewChild('input1') inputEl: ElementRef;
-
+  @ViewChild('input1') input1: ElementRef;
+  @ViewChild('input2') input2: ElementRef;
+  @ViewChild('input3') input3: ElementRef;
+  @ViewChild('input4') input4: ElementRef;
+  @ViewChild('input5') input5: ElementRef;
+  @ViewChild('input6') input6: ElementRef;
+  @ViewChild('2faForm') form: ElementRef;
   constructor(
     private authService: AuthService,
     private store: Store<AppState>
@@ -20,8 +27,28 @@ export class VerificationComponent implements OnInit {
     this.store.select(selectUser).subscribe((user) => (this.user = user));
   }
   public user: User;
-  public timer = 59;
-  ngOnInit(): void {}
+  // public timer = 59;
+  private inputs: ElementRef[];
+  ngOnInit(): void {
+    setTimeout(() => this.input1.nativeElement.focus());
+    timer(0, 1000)
+      .pipe(take(60))
+      .subscribe((timeElapsed) => (this.timer = 60 - timeElapsed));
+  }
+
+  // counter$: Observable<number>;
+  timer = 60;
+
+  ngAfterViewInit() {
+    this.inputs = [
+      this.input1,
+      this.input2,
+      this.input3,
+      this.input4,
+      this.input5,
+      this.input6,
+    ];
+  }
 
   async login(form) {
     const response = await this.authService.login(
@@ -38,11 +65,30 @@ export class VerificationComponent implements OnInit {
     //   }
   }
 
+  calculateNextInput(currentId: string, key: string, inputValue: string) {
+    const currentInput = parseInt(currentId.slice(-1)) - 1;
+    if (key === 'Backspace') {
+      if (currentInput !== 0) {
+        if (inputValue === '') {
+          this.inputs[currentInput - 1].nativeElement.focus();
+          this.inputs[currentInput - 1].nativeElement.value = '';
+        }
+        this.inputs[currentInput - 1].nativeElement.focus();
+      }
+      if (currentInput !== 0) {
+        this.inputs[currentInput - 1].nativeElement.focus();
+      }
+    } else {
+      if (currentInput === 5) {
+        console.log(this.form);
+        console.log('Login');
+      } else {
+        this.inputs[currentInput + 1].nativeElement.focus();
+      }
+    }
+  }
+
   onKey(event: any) {
-    console.log(event);
-    console.log(event.target.id);
-    console.log(event.key);
-    console.log(event.target.value);
-    setTimeout(() => this.inputEl.nativeElement.focus());
+    this.calculateNextInput(event.target.id, event.key, event.target.value);
   }
 }
