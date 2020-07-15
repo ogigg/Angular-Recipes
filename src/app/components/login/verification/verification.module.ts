f0f88c43aa9bd441b391cf29d5702b759a03caa6
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-15 15:46:21 +0200

    Replaced resolver with Guard service

--- src/app/components/login/verification/verification.module.ts
+++ src/app/components/login/verification/verification.module.ts
@@ -4,19 +4,18 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared.module';
 import { VerificationComponent } from './verification.component';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthResolver } from '../auth.resolver';
+import { VerificationGuardService } from './verification-guard.service';
 
 const routes: Routes = [
   {
     path: '',
     component: VerificationComponent,
-    resolve: { auth: AuthResolver },
+    canActivate: [VerificationGuardService],
   },
 ];
 
 @NgModule({
   declarations: [VerificationComponent],
   imports: [CommonModule, RouterModule.forChild(routes), SharedModule],
-  providers: [AuthResolver],
 })
 export class VerificationModule {}
