93cf701a3c1db36750d2f354ba223e20ff243788
Oskar Gierszewski <gierszon10@gmail.com>
2020-07-20 15:25:00 +0200

    Add angularx-social-login

--- src/app/components/login/auth.module.ts
+++ src/app/components/login/auth.module.ts
@@ -1,16 +1,23 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { StoreModule } from '@ngrx/store';
+
+import {
+  SocialLoginModule,
+  SocialAuthServiceConfig,
+  FacebookLoginProvider,
+} from 'angularx-social-login';
+
+import { LoginComponent } from './login.component';
 import { authReducer } from './auth.reducers';
-import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth.effects';
 import { SharedModule } from 'src/app/shared.module';
+
 export const routes: Routes = [
   {
     path: '',
@@ -23,17 +30,31 @@ export const routes: Routes = [
     RouterModule.forChild(routes),
     CommonModule,
     SharedModule,
+    SocialLoginModule,
     StoreModule.forFeature('auth', authReducer),
     EffectsModule.forFeature([AuthEffects]),
   ],
 
   exports: [LoginComponent],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('585082242207135'),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
+  ],
 })
 export class AuthModule {
   static forRoot(): ModuleWithProviders<AuthModule> {
     return {
       ngModule: AuthModule,
-      providers: [],
     };
   }
 }
